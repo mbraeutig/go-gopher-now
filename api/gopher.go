09c8d2efad53eddaaebd1d131dc95c871ffe8f90
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-08 08:04:44 +0200

    Update gopher.go

--- api/gopher.go
+++ api/gopher.go
@@ -1,1 +1,26 @@
 
+package api
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// Gopher prints a gopher.
+func Gopher(w http.ResponseWriter, r *http.Request) {
+	// Read the gopher image file.
+	f, err := os.Open("gophercolor.png")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading file: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	// Write the gopher image to the response writer.
+	if _, err := io.Copy(w, f); err != nil {
+		http.Error(w, fmt.Sprintf("Error writing response: %v", err), http.StatusInternalServerError)
+	}
+	w.Header().Add("Content-Type", "image/png")
+}
