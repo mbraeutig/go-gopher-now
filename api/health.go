d47a267411a6ee18be45f12142a7df8ede3c39d1
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-08 08:26:56 +0200

    Update and rename healthz.go to health.go

--- api/health.go
+++ api/health.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 )
 
-// Healthz - Health Check
-func Healthz(w http.ResponseWriter, r *http.Request) {
+// Health - Health Check
+func Health(w http.ResponseWriter, r *http.Request) {
 	log.Print("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
