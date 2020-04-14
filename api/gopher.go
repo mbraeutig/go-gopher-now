8aa376f52491feb5d8b2f9180b9ded466d70636d
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-14 19:35:54 +0200

    ...

--- api/gopher.go
+++ api/gopher.go
@@ -1,25 +1,32 @@
 package api
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
-	"os"
+
+	"github.com/mbraeutig/go-gopher-now/picture"
 )
 
 // Gopher prints a gopher.
 func Gopher(w http.ResponseWriter, r *http.Request) {
-	// Read the gopher image file.
-	f, err := os.Open("gophercolor.png")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error reading file: %v", err), http.StatusInternalServerError)
-		return
-	}
-	defer f.Close()
-
 	// Write the gopher image to the response writer.
-	if _, err := io.Copy(w, f); err != nil {
+	if _, err := io.Copy(w, bytes.NewReader(picture.Gopher)); err != nil {
 		http.Error(w, fmt.Sprintf("Error writing response: %v", err), http.StatusInternalServerError)
 	}
 	w.Header().Add("Content-Type", "image/png")
+	// Read the gopher image file.
+	// f, err := os.Open("gophercolor.png")
+	// if err != nil {
+	// 	http.Error(w, fmt.Sprintf("Error reading file: %v", err), http.StatusInternalServerError)
+	// 	return
+	// }
+	// defer f.Close()
+
+	// Write the gopher image to the response writer.
+	// if _, err := io.Copy(w, f); err != nil {
+	// 	http.Error(w, fmt.Sprintf("Error writing response: %v", err), http.StatusInternalServerError)
+	// }
+	// w.Header().Add("Content-Type", "image/png")
 }
---
+++ picture/gopher.go
@@ -0,0 +1,23 @@
+package picture
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+var Gopher []byte
+
+func init() {
+	if err := readFile("./public/gophercolor.png", &Gopher); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readFile(filename string, data *[]byte) error {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	*data = b
+	return nil
+}
